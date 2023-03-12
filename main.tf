b534138566adf73f093ce3f12b280611981bd3c3
Filin <filicipa@gmail.com>
2023-03-12 20:13:49 +0200

    Start terraform configuration

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  cloud {
+    organization = "blaze-prod"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0a261c0e5f51090b1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
